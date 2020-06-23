2a399bc82adb46cd6bbf824d7c4cbdad9b9ef06a
loej <joel.martinez@rutgers.edu>
2020-06-23 14:57:21 -0400

    Outlined Comments for Program

---
+++ main.py
@@ -0,0 +1,76 @@
+# OIT CR Scheduler
+
+# Reads CSV and creates Array of Workers #
+#
+# Day of the Week:
+# 0 = Sunday
+# 1 =Monday
+# 2 = Tuesday
+# 3 = Wednesday
+# 4= Thursday
+# 5 = Friday
+# 6 = Saturday
+#
+# Schedule:
+# 0 = ARC
+# 1 = BEST
+# 2 = Kessler
+# 3 = LSM
+#
+# Start/End:
+# 4 digit 24 hour integer
+#
+# i.e. 1:30PM = 1330
+
+#----------------------------------------------------------------------#
+
+# Worker and Shift
+# WORKER:
+# + NetID (String)
+# + Schedule (Array of Shifts)
+# + Supervisor (bool)
+# + addShift(shift): type
+
+# SHIFT:
+# + Location (Integer)
+# + Day of the Week (Int)
+# + Start (Integer)
+# + End (Integer)
+# + method(type): type
+
+
+
+
+
+
+#----------------------------------------------------------------------#
+
+# prioritizeCons()
+# Sorts cons array based on if they work during supervisor hours and how many shifts they have
+#
+# Use sort method based off length of worker Schedule Array
+#
+# Those who work outside of supervisor hours (2000 - 200 next day) can be moved to bottom of list
+
+#----------------------------------------------------------------------#
+
+# Assignment ()
+# Divide lengths of Sup and Cons to get threshold  for each supervisor
+#
+# Go through cons list sequentially. For each cons, go through supervisor list.
+
+#----------------------------------------------------------------------#
+
+# ranking()
+# count overlapping hours
+#  between all supervisors and cons
+#
+# overlapping ARC? +10
+# overlapping LSM? +8
+# overlapping RBHS + 6
+# overlapping BEST? +4
+#
+# difference in number of consultants assigned to supervisor and consultant threshold (x3)
+#
+# return highest ranking supervisor
+
